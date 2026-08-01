1607f333bcc61ddaa808df7b89d9452c3ddd3807
YTAL0 <ytalo122@hotmail.com>
2026-07-31 23:01:26 -0300

    Sua

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,7 @@ import Linked_List.LinkedList;
 public class Main {
     static void main() {
         LinkedList List = new LinkedList();
-
         List.addValue("Baaaaaa");
-        List.addValue("Baba");
-        List.Remove("Baba");
         for(int  i = 0; i < List.getLenght(); i++){
             System.out.println(List.get(i).getValue());
         }
